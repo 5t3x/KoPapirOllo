0fada43bddd90245ddc5f1c81e0f75a4639a7b3c
5t3x <kissmatemail@gmail.com>
2024-11-21 14:22:33 +0100

    konzolos beolvasas

--- src/jatek/KoPapirOllo.java
+++ src/jatek/KoPapirOllo.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package jatek;
 
-/**
- *
- * @author KissMáté(Szf_N_2024_
- */
-public class KoPapirOllo {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class KoPapirOllo {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        /* kerdes amire varjuk a valaszt */
+        System.out.println("egesz");
+        /* eltaroljuk megfelelo tipusban */
+        int egesz = sc.nextInt();
+        /* bekeres eredmenyeinek felhasznalasa */
+        System.out.println("A bekert szam duplaja: " + egesz*2);
+        
+        /*csak akkor kell ha szam utan szoveget kerunk
+        mert a szam utani enter (\r\n) ott marad, ezt olvassa
+        ki.
+        */
+        sc.nextLine();
+        
+        /* kerdes amire varjuk a valaszt */
+        System.out.println("Szoveg");
+        /* eltaroljuk megfelelo tipusban */
+        String szoveg = sc.nextLine();
+        /* bekeres eredmenyeinek felhasznalasa */
+        System.out.println("A bekert szoveg: " + szoveg);
     }
     
 }
