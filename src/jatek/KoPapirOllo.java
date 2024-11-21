82cbdfef6e796055d886f737f2067c54275cffec
5t3x <kissmatemail@gmail.com>
2024-11-21 14:46:13 +0100

    commenteltem a legelso reszt majd ko papir ollo jatekot irtam

--- src/jatek/KoPapirOllo.java
+++ src/jatek/KoPapirOllo.java
@@ -1,30 +1,63 @@
 package jatek;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class KoPapirOllo {
     
     public static void main(String[] args) {
+//        /* kerdes amire varjuk a valaszt */
+//        System.out.println("egesz");
+//        /* eltaroljuk megfelelo tipusban */
+//        int egesz = sc.nextInt();
+//        /* bekeres eredmenyeinek felhasznalasa */
+//        System.out.println("A bekert szam duplaja: " + egesz*2);
+//        
+//        /*csak akkor kell ha szam utan szoveget kerunk
+//        mert a szam utani enter (\r\n) ott marad, ezt olvassa
+//        ki.
+//        */
+//        sc.nextLine();
+//        
+//        /* kerdes amire varjuk a valaszt */
+//        System.out.println("Szoveg");
+//        /* eltaroljuk megfelelo tipusban */
+//        String szoveg = sc.nextLine();
+//        /* bekeres eredmenyeinek felhasznalasa */
+//        System.out.println("A bekert szoveg: " + szoveg);
         Scanner sc = new Scanner(System.in);
-        /* kerdes amire varjuk a valaszt */
-        System.out.println("egesz");
-        /* eltaroljuk megfelelo tipusban */
-        int egesz = sc.nextInt();
-        /* bekeres eredmenyeinek felhasznalasa */
-        System.out.println("A bekert szam duplaja: " + egesz*2);
+        Random random = new Random();
         
-        /*csak akkor kell ha szam utan szoveget kerunk
-        mert a szam utani enter (\r\n) ott marad, ezt olvassa
-        ki.
-        */
-        sc.nextLine();
+        System.out.println("Valassz egyet: (ko/papir/ollo)");
+        String valasztas = sc.nextLine();
+        int randomSzam = random.nextInt(3);
         
-        /* kerdes amire varjuk a valaszt */
-        System.out.println("Szoveg");
-        /* eltaroljuk megfelelo tipusban */
-        String szoveg = sc.nextLine();
-        /* bekeres eredmenyeinek felhasznalasa */
-        System.out.println("A bekert szoveg: " + szoveg);
+        String gepValasztas;
+        if (randomSzam == 0) {
+            gepValasztas = "ko";
+        } else if (randomSzam == 1) {
+            gepValasztas = "papir";
+        } else {
+            gepValasztas = "ollo";
+        }
+        
+        System.out.println("A gep ezt valasztotta: " + gepValasztas);
+        
+        if (valasztas.equals(gepValasztas)) {
+            System.out.println("Dontetlen");
+        } else if (jatekosNyer(valasztas, gepValasztas)) {
+            System.out.println("Jatekos nyer");
+        } else {
+            System.out.println("A gep nyer");
     }
-    
+    }
+    static boolean jatekosNyer(String valasztas, String gepValasztas) {
+        if (valasztas.equals("ko")) {
+            return gepValasztas.equals("ollo");
+        } else if (valasztas.equals("papir")) {
+            return gepValasztas.equals("ko");
+        } else {
+            return gepValasztas.equals("papir");
+    }
+}
 }
